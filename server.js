e40ab6da5a403e5c796f63565a4bad3cd454f547
Reetam101 <reetamraj2@gmail.com>
2022-03-25 16:31:39 +0530

    Pushing to heroku

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use('/api/notes', require('./routes/noteRoutes'))
 
 __dirname = path.resolve()
 if(process.env.NODE_ENV === 'Production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 } else {
     app.get('/', (req, res) => {
